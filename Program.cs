99aa65eb59a33b96fb2975a1d68190c23795d913
sg <srfngrc@outlook.com>
2019-07-03 20:14:28 -0400

    last upload july3rd 8h15pm

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace Ch03Ex02
 {
@@ -12,16 +13,24 @@ namespace Ch03Ex02
         {
             double firstNumber, secondNumber;
             string userName;
+            ConsoleKeyInfo input;
 
             Console.BackgroundColor= ConsoleColor.Red;
             Console.Clear();
             Console.ForegroundColor= ConsoleColor.Blue;
 
-            Console.Beep();
+            //Console.Beep();
+            Console.WriteLine("Press a key, together with Alt, Ctrl, or Shift.");
+            Console.WriteLine("Press Esc to exit.");
+            input = Console.ReadKey(true);
+            Console.WriteLine("soooooo this is: " + input);
+
+            Emailer emailerClass = new Emailer();
+            emailerClass.SendEmail("","","");
 
             Console.WriteLine("Enter your name:");
             userName = Console.ReadLine();
-            Console.WriteLine("Welcome {0}!", userName);
+            Console.WriteLine("Welcome {0, 26}!", userName);
             Console.WriteLine("Now give me a number:");
             firstNumber = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Now give me another number:");
@@ -43,4 +52,15 @@ namespace Ch03Ex02
             Console.ReadKey();
         }
     }
+}
+
+namespace Utilities
+{
+    class Emailer
+    {
+        public void SendEmail(string to, string from, string message)
+        {
+            //send some emails
+        }
+    }   
 }
